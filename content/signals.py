bb11d28caad6fb9751e0ad23f0dadcaa92cf37ea
AnneEberhard <anne.eberhard@gmx.net>
2024-03-19 11:38:25 +0100

    added video on save and delete

---
+++ content/signals.py
@@ -0,0 +1,21 @@
+import os
+from .models import Video
+from django.dispatch import receiver
+from django.db.models.signals import post_save, post_delete
+
+@receiver(post_save, sender=Video)
+def video_pos_save(sender, instance, created, **kwargs):
+    print('Video')
+    if created:
+        print('new video created')
+
+
+@receiver(post_delete, sender=Video)
+def auto_delete_file_on_delete(sender, instance, **kkwargs):
+    """
+    Deletes file from filesystem
+    when corresponing 'Video' onject is deleted
+    """
+    if instance.video_file:
+        if os.path.isfile(instance.video_file.path):
+            os.remove(instance.video_file.path)
