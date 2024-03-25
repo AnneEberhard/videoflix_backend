7982e255fda64692b62d4c233a783fafd5fb3bc8
AnneEberhard <anne.eberhard@gmx.net>
2024-03-25 16:55:39 +0100

    added renaming and deleting of converted videos

--- content/signals.py
+++ content/signals.py
@@ -23,3 +23,15 @@ def auto_delete_file_on_delete(sender, instance, **kkwargs):
     if instance.video_file:
         if os.path.isfile(instance.video_file.path):
             os.remove(instance.video_file.path)
+    
+    if instance.video_file:
+        base_name, ext = os.path.splitext(instance.video_file.path)
+        converted_file_path = f"{base_name}_480p.mp4"
+        if os.path.isfile(converted_file_path):
+            os.remove(converted_file_path)
+
+    if instance.video_file:
+        base_name, ext = os.path.splitext(instance.video_file.path)
+        converted_file_path = f"{base_name}_720p.mp4"
+        if os.path.isfile(converted_file_path):
+            os.remove(converted_file_path)
