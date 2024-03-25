63f85a36e7bf995ee255b6dab58e15940f01b9a6
AnneEberhard <anne.eberhard@gmx.net>
2024-03-25 18:32:37 +0100

    added ffmpeg simple

--- content/tasks.py
+++ content/tasks.py
@@ -4,13 +4,14 @@ import subprocess
 def convert480p(source):   
     base_name, ext = os.path.splitext(source)
     new_file_name = base_name + '_480p.mp4'
-    
-    cmd = 'C:\\usr\\ffmpeg\\bin\\ffmpeg.exe -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file_name)    
+    cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file_name)
+    # cmd = 'C:\\usr\\ffmpeg\\bin\\ffmpeg.exe -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file_name)    
     run = subprocess.run(cmd, capture_output=True)
 
 
 def convert720p(source):    
     base_name, ext = os.path.splitext(source)
     new_file_name = base_name + '_720p.mp4'
-    cmd = 'C:\\usr\\ffmpeg\\bin\\ffmpeg.exe -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file_name)    
+    cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file_name)    
+    # cmd = 'C:\\usr\\ffmpeg\\bin\\ffmpeg.exe -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file_name)    
     run = subprocess.run(cmd, capture_output=True)
