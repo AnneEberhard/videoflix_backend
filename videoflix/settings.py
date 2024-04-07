3253aa41fd2eaf3898bc047c979c256b72e4e5a1
AnneEberhard <anne.eberhard@gmx.net>
2024-04-07 12:55:12 +0200

    added style

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -168,9 +168,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [BASE_DIR / "static/style"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
