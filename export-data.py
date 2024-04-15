eb78252e962d9f6b14458a777e516096143e40d0
AnneEberhard <anne.eberhard@gmx.net>
2024-04-15 18:27:14 +0200

    export-data.py added

--- export-data.py
+++ export-data.py
@@ -1,5 +1,6 @@
 import os
 import django
+import json
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'videoflix.settings')
 
@@ -17,7 +18,12 @@ def download_json():
     response = HttpResponse(json_data, content_type='application/json')
 
     response['Content-Disposition'] = 'attachment; filename="export.json"'
-    print(json_data)
+    # print(json_data)
+    parsed = json.loads(json_data)
+    # print(json.dumps(parsed, indent=4))
+    text_file = open("Video.json", "w")
+    text_file.write(json.dumps(parsed, indent=4))
+    text_file.close()
 
     return response
 
@@ -25,4 +31,3 @@ def download_json():
 download_response = download_json()
 print(download_response)
 
-#return download_response
