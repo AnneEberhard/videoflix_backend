e0b28554e033c311e4806b67122a6e77d239013d
AnneEberhard <anne.eberhard@gmx.net>
2024-05-11 10:35:30 +0200

    neue upbat

--- up.bat
+++ up.bat
@@ -1,4 +1,4 @@
 git add . 
 git commit -m "%*"
 git push
-ssh annee@34.34.178.94 "cd /home/anne_eberhard74/projects/videoflix_backend/ && sudo git pull"
+ssh annee@34.34.178.94 "cd /home/anne_eberhard74/projects/videoflix_backend/ && git pull"
