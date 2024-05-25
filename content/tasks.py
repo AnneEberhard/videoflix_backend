86f08737dd5ee8637c360c6bc509bda56613c3d6
AnneEberhard <anne.eberhard@gmx.net>
2024-05-25 10:07:08 +0200

    shell

--- content/tasks.py
+++ content/tasks.py
@@ -11,7 +11,7 @@ def convert480p(source):
     base_name, ext = os.path.splitext(source)
     new_file_name = base_name + '_480p.mp4'
     cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file_name)
-    subprocess.run(cmd, capture_output=True)
+    subprocess.run(cmd, shell=True, capture_output=True)
 
 
 def convert720p(source):
@@ -23,4 +23,4 @@ def convert720p(source):
     base_name, ext = os.path.splitext(source)
     new_file_name = base_name + '_720p.mp4'
     cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, new_file_name)
-    subprocess.run(cmd, capture_output=True)
+    subprocess.run(cmd, shell=True, capture_output=True)
